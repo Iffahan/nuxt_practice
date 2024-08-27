ca6fe3b1a2acf667886f1c9f30b29ac9431dd292
อิฟฟาฮาน สุขสุวรรณ <jackcopsluncher@gmail.com>
2024-08-28 00:41:30 +0700

    second commit with project file

---
+++ pages/index.vue
@@ -0,0 +1,15 @@
+<template>
+  <div class="flex items-center justify-center min-h-screen bg-gray-100">
+    <h1 class="text-4xl font-bold text-blue-600">
+      Hello World
+    </h1>
+  </div>
+</template>
+
+<script lang="ts">
+import Vue from 'vue'
+
+export default Vue.extend({
+  name: 'IndexPage',
+})
+</script>
