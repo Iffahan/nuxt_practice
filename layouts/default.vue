54aa20c1ca8e3fd2ae815f02e87194d79e6fb263
อิฟฟาฮาน สุขสุวรรณ <jackcopsluncher@gmail.com>
2024-08-28 01:24:42 +0700

    add layouts, set default layout

---
+++ layouts/default.vue
@@ -0,0 +1,13 @@
+<template>
+  <div>
+    <header class="p-4 bg-gray-800 text-white">My App Header</header>
+    <nuxt />
+    <footer class="p-4 bg-gray-800 text-white mt-8">My App Footer</footer>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'DefaultLayout',
+}
+</script>
